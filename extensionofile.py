e9d060c6d0a27d0c5b0ebed0e9d24e8ce792899a
Diti03 <106997378+Diti03@users.noreply.github.com>
2022-07-09 11:34:53 +0530

    Add files via upload

---
+++ extensionofile.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  9 11:31:01 2022
+
+@author: ditij
+"""
+
+filename= input("Input the filename:")
+f_extns= filename.split(".")
+print("The extension of the file is:"+repr(f_extns[-1]))
